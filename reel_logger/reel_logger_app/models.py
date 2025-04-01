bce1429b2992b040e2be878ea05ba65a525b087c
Walle10-0 <lukemauro900@gmail.com>
2025-03-31 22:59:31 -0400

    hash computed

--- reel_logger/reel_logger_app/models.py
+++ reel_logger/reel_logger_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from hashlib import md5 as hash
 
 from reel_logger.settings import MEDIA_ROOT
 
@@ -6,14 +7,20 @@ def get_footage_root():
     return MEDIA_ROOT + "footage/"
 
 class Footage(models.Model):
-    path = models.FilePathField(path=get_footage_root, blank=True, null=True, recursive=True)
-    hash = models.CharField(max_length=32)
-    length = models.DurationField()
-    has_audio = models.BooleanField()
-    has_video = models.BooleanField()
-    notes = models.TextField()
+    path = models.FilePathField(path=get_footage_root, blank=False, null=False, recursive=True, unique=True)
+    hash = models.CharField(max_length=32, blank=True, editable=False)
+    length = models.DurationField(blank=True, default=0)
+    has_audio = models.BooleanField(default=False)
+    has_video = models.BooleanField(default=False)
+    notes = models.TextField(blank=True)
 
     takes = None
+
+    def save(self, *args, **kwargs):
+        print(self.path)
+        with open(self.path, "rb") as file:
+            self.hash = hash(file.read()).hexdigest()
+        super(Footage, self).save(*args, **kwargs)
 
 class Comment(models.Model):
     footage = models.ForeignKey(Footage, on_delete=models.CASCADE)
@@ -21,14 +28,14 @@ class Comment(models.Model):
     comment = models.TextField()
 
 class Scene(models.Model):
-    script_number = models.PositiveSmallIntegerField()
+    script_number = models.PositiveSmallIntegerField(unique=True)
     title = models.CharField(max_length=128)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
 class Shot(models.Model):
     scene = models.ForeignKey(Scene, on_delete=models.CASCADE)
     shot = models.CharField(max_length=64)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
 class Take(models.Model):
     footage = models.ManyToManyRel(field="takes", to=Footage)
@@ -39,7 +46,7 @@ class Take(models.Model):
     marked_shot = models.CharField(max_length=64)
     marked_take = models.PositiveSmallIntegerField()
     rating = models.SmallIntegerField()
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
 class FootageUpload(models.Model):
     footage = models.FileField(upload_to='footage/unlogged/')
